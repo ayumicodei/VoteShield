26eeab1bba3c9da2ca9cf12f6eecf2e20c05788f
ayumicodei <ebsfpx077@outlook.com>
2025-12-26 13:04:58 +0800

    Set up wagmi and contract config

---
+++ src/src/config/wagmi.ts
@@ -0,0 +1,12 @@
+import { createConfig, createStorage, http, noopStorage } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { injected } from 'wagmi/connectors';
+
+export const config = createConfig({
+  chains: [sepolia],
+  connectors: [injected()],
+  transports: {
+    [sepolia.id]: http(),
+  },
+  storage: createStorage({ storage: noopStorage }),
+});
